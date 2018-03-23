fe4f75e77bd58a194434be984e36417649484d0e
Ronaj5 <37316864+Ronaj5@users.noreply.github.com>
2018-03-22 19:53:36 -0500

    NumbersRight

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -8,10 +8,10 @@ public class P12_NumbersLeft {
 
 	public static void main(String[] args) {
 		 for( int i = 1; i<= 5; i++ ) {
-			 for( int j = 1; j<= i; j++ ) {
+			 for( int j = 1; j<=i; j++ ) {
 				 System.out.print(i);
 			 }
-	            System.out.println();
-	}
+			System.out.println();
+		}
 	}
 }
--- src/P13_NumbersRight.java
+++ src/P13_NumbersRight.java
@@ -1,17 +1,31 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Rona Davis	
+  	Date: March 21, 2018
+  	
+    1
+   22
+  333
+ 4444
+55555
+
+  	
 */
 
 public class P13_NumbersRight {
 
-		public static void main(String[] args) {
-			 for( int i = 5; i>=0; i--) {
-				 for( int j = 1; j<=i; j++) {
-					 System.out.print(i);
-				 }
-		            System.out.println();
+	public static void main(String[] args) {
+		for( int i = 1; i<= 5; i++ ) {
+			 
+			for(int j = 1; j<= (5-i); j++) {
+				System.out.print(" ");
+			
+	
+			}
+			for( int j = 1; j<=i; j++ ) {
+				 System.out.print(i);
+			 }
+			System.out.println();
 		}
-		}
+	}
 }
